9e803053bbc5958a5392491ec66b29e2044d6fef
Justina Kuchta <justyna.kuchta@gmail.com>
2019-10-11 14:30:40 -0400

    almost slapped cards on the DOM

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -2,6 +2,8 @@ const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 
+const trainersDiv = document.querySelector("#trainers")
+
 config = {
     method: 'POST',
     headers: {
@@ -16,3 +18,33 @@ config = {
 }
 
 fetch(POKEMONS_URL, config)
+
+let dataCounter = 1;
+
+fetch(TRAINERS_URL).then(response => response.json()).then(trainers => {
+    trainers.forEach(trainer => {
+        let div = document.createElement("div")
+        div.className = "card"
+        div.attributes["data-id"] = dataCounter++
+        let trainerName = document.createElement("p")
+        trainerName.innerText = trainer.name
+        div.appendChild(trainerName)
+        const button = document.createElement("button")
+        button["data-trainer-id"] = trainer.id
+        button.innerText = "Add Pokemon"
+        div.appendChild(button)
+        const ul = document.createElement("ul")
+        div.appendChild(ul)
+        trainer.pokemons.forEach(pokemon => {
+            let li = document.createElement("li")
+            li.innerText = `${pokemon.nickname} (${pokemon.species})`
+            let pokeButton = document.createElement("button")
+            pokeButton.class = "release"
+            pokeButton["data-pokemon-id"] = pokemon.id
+            pokeButton.innerText = "Release"
+            li.appendChild(pokeButton)
+            ul.appendChild(li)
+        })
+        trainersDiv.appendChild(div)
+    })
+})
